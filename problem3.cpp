e6466aa582091c775a4485cbf4417c529345e9a3
Andreas Kind <a.kind@neusta.de>
2020-07-21 13:19:53 +0200

    initial commit

--- problem3.cpp
+++ problem3.cpp
@@ -3,11 +3,6 @@
 
 using namespace std;
 
-
-bool isEven(double long sq) {
-    return sq - ceil(sq) == 0;
-}
-
 /*
  * The prime factors of 13195 are 5, 7, 13 and 29.
  * What is the largest prime factor of the number 600851475143 ?
